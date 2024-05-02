eafbbe948957c88670e7a54672e413d822b1fca7
MichPixik <michaelzdvoran@gmail.com>
2024-05-02 13:31:08 +0200

    PIKSA.POLOLETKA.HOTOVO

--- index.js
+++ index.js
@@ -22,5 +22,26 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     });
   });
+
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const stars = document.querySelectorAll(".star");
+  
+    stars.forEach(function(star) {
+      star.addEventListener("click", function() {
+        const value = parseInt(star.getAttribute("data-value"));
+  
+        // Označení všech hvězd do které uživatel kliknul
+        for (let i = 0; i < stars.length; i++) {
+          if (i < value) {
+            stars[i].classList.add("active");
+          } else {
+            stars[i].classList.remove("active");
+          }
+        }
+      });
+    });
+  });
+  
   
 
